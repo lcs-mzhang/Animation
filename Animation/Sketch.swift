adeb0421836e3099fd36c3e0c68c6f827b708938
Zhang Matt <mzhang@lcs.on.ca>
2017-10-25 11:22:24 -0400

    Completed bouncing ball challenge

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,14 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
     
+    var dx : Int //Difference in x
+    var dy : Int
+    
+    var speed : Int
+    
+
     // This function runs once
     override init() {
         
@@ -16,19 +23,47 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = random(from: 25, toButNotIncluding: 475)
+        y = random(from: 25, toButNotIncluding: 475)
         
+        speed = 10
+        
+        dy = speed
+        dx = speed
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        //Clear the background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        canvas.fillColor = Color.black
+        
+        x += dx
+        y += dy
+        
+        if x > 475
+        {
+            dx = -speed
+        }
+        if x < 25
+        {
+            dx = speed
+        }
+        if y > 475
+        {
+            dy = -speed
+        }
+        if y < 25
+        {
+            dy = speed
+        }
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
         
     }
     
 }
+
