064c1779abddc6cf7df228d0cd72eca48343fbc9
Zhang Matt <mzhang@lcs.on.ca>
2017-10-25 10:19:23 -0400

    Made the ball change colour and bounce off walls

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,11 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    //Change in position
+    var dx : Int //Difference in x
+    
+    var speed : Int
+    
     // This function runs once
     override init() {
         
@@ -16,15 +21,33 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+
+        speed = 10
         
+        dx = speed
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        //Clear the background
+        
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        canvas.fillColor = Color.init(hue: x, saturation: 100, brightness: 100, alpha: 100)
         // Change position
-        x += 1
+        x += dx
+        
+        if x > 475
+        {
+            dx = -speed
+        }
+       
+        if x < 25
+        {
+            dx = speed
+        }
         
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
