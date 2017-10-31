6d06dcfbef1c659e66fa0472d29825fda00b52ed
Zhang Matt <mzhang@lcs.on.ca>
2017-10-31 11:02:28 -0400

    Made a smiley trash bag with a flickering eye socket.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -1,74 +1,77 @@
 import Foundation
 
 class Sketch : NSObject {
-    
-    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
-    //       Therefore, the line immediately below must always be present.
+
     let canvas : Canvas
+    var rand : Int
     
-    // Position of circle
-    var x : Int
-    
-    // This function runs once
     override init() {
-        
-        // Create canvas object – specify size
-        canvas = Canvas(width: 800, height: 600)
-        
-        // Set starting position
-        x = 250
+    
+        canvas = Canvas(width: 1000, height: 1000)
+        rand = 1
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
+        // Create canvas object – specify size
         
-        canvas.fillColor = Color.white
-        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
-        
-        x += 1
         
         canvas.drawShapesWithBorders = false
         
-        // Move the origin to the middle of the canvasand
-        canvas.translate(byX: x, byY: 300)
-
-        
-        // Draw a bat relative to the origin
         canvas.fillColor = Color.black
-        
-        // First make an ellipse to form the body
-        canvas.drawEllipse(centreX: 0, centreY: 0, width: 200, height: 100)
-        
-        // Now "cut out" the wings and head by overlapping circles
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
+        canvas.translate(byX: canvas.width/2, byY: canvas.height/2)
+        //HEAD
         canvas.fillColor = Color.white
+        canvas.drawEllipse(centreX: 0, centreY: 150, width: 600, height: 600)
         
-        // Underside of wings
-        canvas.drawEllipse(centreX: -70, centreY: -10, width: 50, height: 50) // left
-        canvas.drawEllipse(centreX: -35, centreY: -10, width: 50, height: 50) // left middle
-        canvas.drawEllipse(centreX: 35, centreY: -10, width: 50, height: 50) // right middle
-        canvas.drawEllipse(centreX: 70, centreY: -10, width: 50, height: 50) // right
-        // Further down
-        canvas.drawEllipse(centreX: 0, centreY: -50, width: 50, height: 100) // middle
-        // Get rid of rest further down
-        canvas.drawRectangle(centreX: 0, centreY: -35, width: 200, height: 30)
         
-        // Now add the head
+        //mouth
         canvas.fillColor = Color.black
-        var headVertices : [NSPoint] = []
-        headVertices.append(NSPoint(x: -40, y: 40))
-        headVertices.append(NSPoint(x: 40, y: 40))
-        headVertices.append(NSPoint(x: 30, y: 80))
-        headVertices.append(NSPoint(x: 20, y: 60))
-        headVertices.append(NSPoint(x: -20, y: 60))
-        headVertices.append(NSPoint(x: -30, y: 80))
-        headVertices.append(NSPoint(x: -40, y: 40)) // end where we started
-        canvas.drawCustomShape(with: headVertices)
+        canvas.drawEllipse(centreX: 0, centreY: 0, width: 400, height: 200)
+        canvas.fillColor = Color.white
+        //teeth
         
-        canvas.fillColor = Color.red
-        canvas.drawEllipse(centreX: -10, centreY: 45, width: 10, height: 10)
-        canvas.drawEllipse(centreX: 10, centreY: 45, width: 10, height: 10)
-
+        canvas.drawEllipse(centreX: -125, centreY: -25, width: 50, height: 100)
+        canvas.drawEllipse(centreX: -25, centreY: -50, width: 50, height: 100)
+        canvas.drawEllipse(centreX: 25, centreY: -50, width: 50, height: 100)
+        canvas.drawEllipse(centreX: 125, centreY: -25, width: 50, height: 100)
+        canvas.drawEllipse(centreX: -75, centreY: -37, width: 50, height: 100)
+        canvas.drawEllipse(centreX: 75, centreY: -37, width: 50, height: 100)
+        canvas.drawEllipse(centreX: -175, centreY: 100, width: 50, height: 100)
+        canvas.drawEllipse(centreX: 175, centreY: 100, width: 50, height: 100)
+        
+        canvas.drawEllipse(centreX: 0, centreY: 100, width: 450, height: 200)
+        
+        //nose
+        canvas.fillColor = Color.black
+        var nose : [NSPoint] = []
+        nose.append(NSPoint(x: 0, y: 150))
+        nose.append(NSPoint(x: 50, y: 50))
+        nose.append(NSPoint(x: -50, y: 50))
+        canvas.drawCustomShape(with: nose)
+        
+        //eyes
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: -150, centreY: 200, width: 150, height: 150)
+        canvas.drawEllipse(centreX: 150, centreY: 200, width: 150, height: 150)
+        canvas.fillColor = Color.white
+        canvas.drawEllipse(centreX: -150, centreY: 200, width: 50, height: 50)
+        canvas.drawEllipse(centreX: 150, centreY: 200, width: 50, height: 50)
+        
+       
+rand = random(from: 0, toButNotIncluding: 11)
+        if rand == 1
+        {
+            canvas.fillColor = Color.init(hue: 192, saturation: 100, brightness: 50, alpha: 100)
+        }
+        else
+        {
+            canvas.fillColor = Color.white
+        }
+canvas.drawEllipse(centreX: 150, centreY: 200, width: 50, height: 50)
+      
     }
     
 }
