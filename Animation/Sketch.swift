9b5e313490bc2376ba9806d391d78fa04b1357e5
Zhang Matt <mzhang@lcs.on.ca>
2017-10-31 09:14:11 -0400

    Made some eyes

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -13,7 +13,7 @@ class Sketch : NSObject {
     override init() {
         
         // Create canvas object – specify size
-        canvas = Canvas(width: 500, height: 500)
+        canvas = Canvas(width: 800, height: 600)
         
         // Set starting position
         x = 250
@@ -23,12 +23,52 @@ class Sketch : NSObject {
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
+        
         x += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawShapesWithBorders = false
         
+        // Move the origin to the middle of the canvasand
+        canvas.translate(byX: x, byY: 300)
+
+        
+        // Draw a bat relative to the origin
+        canvas.fillColor = Color.black
+        
+        // First make an ellipse to form the body
+        canvas.drawEllipse(centreX: 0, centreY: 0, width: 200, height: 100)
+        
+        // Now "cut out" the wings and head by overlapping circles
+        canvas.fillColor = Color.white
+        
+        // Underside of wings
+        canvas.drawEllipse(centreX: -70, centreY: -10, width: 50, height: 50) // left
+        canvas.drawEllipse(centreX: -35, centreY: -10, width: 50, height: 50) // left middle
+        canvas.drawEllipse(centreX: 35, centreY: -10, width: 50, height: 50) // right middle
+        canvas.drawEllipse(centreX: 70, centreY: -10, width: 50, height: 50) // right
+        // Further down
+        canvas.drawEllipse(centreX: 0, centreY: -50, width: 50, height: 100) // middle
+        // Get rid of rest further down
+        canvas.drawRectangle(centreX: 0, centreY: -35, width: 200, height: 30)
+        
+        // Now add the head
+        canvas.fillColor = Color.black
+        var headVertices : [NSPoint] = []
+        headVertices.append(NSPoint(x: -40, y: 40))
+        headVertices.append(NSPoint(x: 40, y: 40))
+        headVertices.append(NSPoint(x: 30, y: 80))
+        headVertices.append(NSPoint(x: 20, y: 60))
+        headVertices.append(NSPoint(x: -20, y: 60))
+        headVertices.append(NSPoint(x: -30, y: 80))
+        headVertices.append(NSPoint(x: -40, y: 40)) // end where we started
+        canvas.drawCustomShape(with: headVertices)
+        
+        canvas.fillColor = Color.red
+        canvas.drawEllipse(centreX: -10, centreY: 45, width: 10, height: 10)
+        canvas.drawEllipse(centreX: 10, centreY: 45, width: 10, height: 10)
+
     }
     
 }
