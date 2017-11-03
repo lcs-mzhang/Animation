9255271290cca4c8d9226bd9996e0d95fc6b2a57
Zhang Matt <mzhang@lcs.on.ca>
2017-11-03 10:53:24 -0400

    such cool
    much wow

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Sketch : NSObject {
     let canvas : Canvas
-
+    
     var x : Int
     var y : Int
     var x1 : Int
@@ -28,8 +28,8 @@ class Sketch : NSObject {
         
         x = random(from: 0, toButNotIncluding: 500)
         y = random(from: 0, toButNotIncluding: 500)
-        x1 = 0
-        y1 = 0
+        x1 = random(from: 0, toButNotIncluding: 500)
+        y1 = random(from: 0, toButNotIncluding: 500)
         
         speed = 5
         
@@ -46,13 +46,13 @@ class Sketch : NSObject {
     func draw() {
         
         //Clear the background
-//        canvas.drawShapesWithFill = true
-//        canvas.fillColor = Color.white
-//        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
-//
-//        canvas.drawShapesWithFill = false
-//        canvas.borderColor = Color.black
-       
+        //        canvas.drawShapesWithFill = true
+        //        canvas.fillColor = Color.white
+        //        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        //
+        //        canvas.drawShapesWithFill = false
+        //        canvas.borderColor = Color.black
+        
         x1 += dx1
         x += dx
         y1 += dy1
@@ -91,27 +91,26 @@ class Sketch : NSObject {
             dy1 = speed
         }
         
-//        canvas.drawEllipse(centreX: x, centreY: y, width: 1, height: 1)
-//        canvas.drawEllipse(centreX: x, centreY: y, width: 100, height: 100)
-//        canvas.drawEllipse(centreX: x1, centreY: y1, width: 1, height: 1)
-//        canvas.drawEllipse(centreX: x1, centreY: y1, width: 200, height: 200)
-
-//        distance = sqrt(pow(x1-x, 2) + pow(250-y,2))
+        //        canvas.drawEllipse(centreX: x, centreY: y, width: 1, height: 1)
+        //        canvas.drawEllipse(centreX: x, centreY: y, width: 100, height: 100)
+        //        canvas.drawEllipse(centreX: x1, centreY: y1, width: 1, height: 1)
+        //        canvas.drawEllipse(centreX: x1, centreY: y1, width: 200, height: 200)
+        //        distance = sqrt(pow(x1-x, 2) + pow(250-y,2))
         
         let aSquared = (x1-x)*(x1-x)
         let bSquared = (y1-y)*(y1-y)
         distance = (sqrt(Double(aSquared)+Double(bSquared)))
         
-        if distance < 250
+        if distance < 500
         {
-        canvas.lineColor = Color.init(hue: y, saturation: 100, brightness: 100, alpha: 100)
-        canvas.drawLine(fromX: x1, fromY: y1, toX: x, toY: y)
+            canvas.lineColor = Color.init(hue: y, saturation: 100, brightness: 100, alpha: 100)
+            canvas.drawLine(fromX: x1, fromY: y1, toX: x, toY: y)
         }
         
-
         
-
-
+        
+        
+        
         
     }
     
