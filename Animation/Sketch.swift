5d8b6199f53a1fe136c08acb66c1883fcfcd7a3f
Zhang Matt <mzhang@lcs.on.ca>
2017-11-08 11:24:28 -0500

    Completed the challenge!

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -14,8 +14,10 @@ class Sketch : NSObject {
     // Position of circle
     var x : Degrees
     var y : Int
-    var x2 : Int
+    var x2 : Double
     var y2 : Int
+    var x3 : Double
+    var y3 : Int
     
     // This function runs once
     override init() {
@@ -28,6 +30,8 @@ class Sketch : NSObject {
         y = 0
         x2 = 0
         y2 = 0
+        x3 = 0
+        y3 = 0
 
         canvas.drawShapesWithBorders = false
         
@@ -40,7 +44,7 @@ class Sketch : NSObject {
         
         // Change position
         x += 1
-        x2 += 1
+        
         
         y = Int(100*sin(Double(x.toRadians()*2)))
         for k in stride(from: 0, to: 360, by: 20)
@@ -54,9 +58,18 @@ class Sketch : NSObject {
             else
             {
                 canvas.fillColor = Color.white
-                
-                y2 = x2 * x2 + k
-                canvas.drawEllipse(centreX: x2, centreY: y2, width: 10, height: 10)
+                x2 += 1
+                x3 += 1
+                for i : Double in stride(from: 360, to: 0, by: -20)
+                {
+                    let a : Double = -6/90
+                    let y2 = a * (pow(x2-150,2)) + i
+                    let y3 = a * (pow(x3-210,2)) + i
+                    canvas.drawEllipse(centreX: Int(x2), centreY: Int(y2), width: 5, height: 5)
+                    canvas.drawEllipse(centreX: Int(x3), centreY: Int(y3), width: 5, height: 5)
+                }
+
+
             }
 
         }
